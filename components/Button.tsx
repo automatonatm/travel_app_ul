ce8ddf318f15341a1af42bc740b5db94faca0e55
Automaton <46001539+automatonatm@users.noreply.github.com>
2023-12-22 22:15:42 +0100

    Get App completed

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,11 +5,12 @@ interface ButtonProps {
   icon?: string;
   type?: 'button' | 'submit';
   varriant: string;
+  full?: boolean
 }
 
-const Button = ({ title, icon, varriant, type = "button" }: ButtonProps) => {
+const Button = ({ title, icon, varriant, type = "button", full }: ButtonProps) => {
   return (
-    <button type={type} className={`flexCenter gap-3 rounded-full border ${varriant}`}>
+    <button type={type} className={`flexCenter gap-3 rounded-full border ${varriant} ${full && 'w-full'}`}>
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap">{title}</label>
     </button>
